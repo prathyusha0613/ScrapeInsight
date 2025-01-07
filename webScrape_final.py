83595bae75281cb665f76507ffd76fa300644820
Prathyusha A <141655336+prathyusha0613@users.noreply.github.com>
2025-01-07 15:56:02 +0530

    Update webScrape_final.py

--- webScrape_final.py
+++ webScrape_final.py
@@ -32,10 +32,10 @@ def analyze_and_visualize(words_list):
     word_counts = Counter(words_list)
     df = pd.DataFrame.from_dict(word_counts, orient='index', columns=['Frequency'])
     df = df.sort_values(by='Frequency', ascending=False)
-    # Convert the index (words) to string to ensure numeric values
+    # converting the index (words) to string to ensure numeric values
     df.index = df.index.astype(str)
     if not df.empty:
-        # Plot a line chart using word frequencies
+        # line chart using word frequencies
         df['Frequency'].plot(kind='line', marker='o', linestyle='-', color='green')
         plt.title('Word Frequency Distribution')
         plt.xlabel('Words')
@@ -52,13 +52,13 @@ if __name__ == "__main__":
     # Flatten the results from parallel execution
     sentences_list = [sentence for sublist in sentences_result for sentence in sublist]
     words_list = [word for sublist in words_result for word in sublist]
-    # Display sentences containing the specified word
+  
     print("\nSentences containing the specified word:\n")
     for sentence in sentences_list:
         print(sentence)
-    # Display words of the specified length
+    
     print("\nWords of the specified length:\n")
     for word in words_list:
         print(word)
-    # Analyze and visualize word frequency
+    
     analyze_and_visualize(words_list)
